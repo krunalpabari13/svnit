b6b61fb1ff627bf5baddd1d6f49b18c951194a84
Krunal-Pabari <100471559+krunalpabari13@users.noreply.github.com>
2023-02-19 02:01:05 +0530

    Add files via upload

---
+++ Mainimg2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.css'
+export default function Mainimg2() {
+  return (
+    <div className='text-center ' style={{marginTop:'2%'}}>
+        <img src={require('./four.png')} className='img-fluid container mt-4 ' ></img>
+    </div>
+  )
+}
---
+++ Text2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './index.css'
+export default function Text2() {
+  return (
+    <div className='text-center ' style={{color:'white' ,fontSize:'3vw',marginTop:'6%'}}><span style={{color:'#1DF632',fontWeight:'bolder'}}>CHARGER</span> IS AT YOUR DISPOSAL. <div className='mt-4'> ANY TIME TO RECHARGE YOUR VEHICLE</div></div>
+  )
+}
